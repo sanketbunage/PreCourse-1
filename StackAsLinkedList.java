71846a6c3319b3e8f4d2a9ccfb213b53c86f77d7
Sanket <sanketsbunage@gmail.com>
2025-05-12 17:49:53 -0700

    Precourse 1- Done

---
+++ StackAsLinkedList.java
@@ -0,0 +1,69 @@
+public class StackAsLinkedList { 
+  
+    StackNode root; 
+  
+    static class StackNode { 
+        int data; 
+        StackNode next; 
+  
+        StackNode(int data) 
+        { 
+            //Constructor here 
+            this.data = data;
+            this.next = null;
+        } 
+    } 
+    
+	
+    public boolean isEmpty() 
+    { 
+        return root == null;
+    } 
+  
+    public void push(int data) 
+    { 
+        StackNode newNode = new StackNode(data);
+        newNode.next = root;
+        root = newNode;
+    } 
+  
+    public int pop() 
+    { 	
+	//If Stack Empty Return 0 and print "Stack Underflow"
+        //Write code to pop the topmost element of stack.
+	//Also return the popped element 
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int popped = root.data;
+            root = root.next;
+            return popped;
+        }
+    } 
+  
+    public int peek() 
+    { 
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return 0;
+        } else {
+            return root.data;
+        }
+    } 
+  
+	//Driver code
+    public static void main(String[] args) 
+    { 
+  
+        StackAsLinkedList sll = new StackAsLinkedList(); 
+  
+        sll.push(10); 
+        sll.push(20); 
+        sll.push(30); 
+  
+        System.out.println(sll.pop() + " popped from stack"); 
+  
+        System.out.println("Top element is " + sll.peek()); 
+    } 
+} 
